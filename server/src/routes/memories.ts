282b658c2574f62bde0c0f920bcc67a26d00ac5e
SamyrOR <saamyr@live.com>
2023-05-19 00:04:18 -0300

    lesson 3

--- server/src/routes/memories.ts
+++ server/src/routes/memories.ts
@@ -1,10 +1,17 @@
 import { FastifyInstance } from 'fastify';
 import { z } from 'zod';
-import { prisma } from '../lib/prisma';
+import { prisma } from './../lib/prisma';
 
 export async function memoriesRoutes(app: FastifyInstance) {
-    app.get('/memories', async () => {
+    app.addHook('preHandler', async (request) => {
+        await request.jwtVerify();
+    });
+
+    app.get('/memories', async (request) => {
         const memories = await prisma.memory.findMany({
+            where: {
+                id: request.user.sub,
+            },
             orderBy: {
                 createdAt: 'asc',
             },
@@ -19,7 +26,7 @@ export async function memoriesRoutes(app: FastifyInstance) {
         });
     });
 
-    app.get('/memories/:id', async (request) => {
+    app.get('/memories/:id', async (request, response) => {
         const paramsSchema = z.object({
             id: z.string().uuid(),
         });
@@ -29,6 +36,10 @@ export async function memoriesRoutes(app: FastifyInstance) {
                 id,
             },
         });
+
+        if (!memory.isPublic && memory.userId !== request.user.sub) {
+            return response.status(401).send();
+        }
 
         return memory;
     });
@@ -47,14 +58,14 @@ export async function memoriesRoutes(app: FastifyInstance) {
                 content,
                 coverUrl,
                 isPublic,
-                userId: '023faedb-e63f-4ba3-ab54-d04972424f07',
+                userId: request.user.sub,
             },
         });
 
         return memory;
     });
 
-    app.put('/memories/:id', async (request) => {
+    app.put('/memories/:id', async (request, response) => {
         const paramsSchema = z.object({
             id: z.string().uuid(),
         });
@@ -65,7 +76,15 @@ export async function memoriesRoutes(app: FastifyInstance) {
             isPublic: z.coerce.boolean().default(false),
         });
         const { content, coverUrl, isPublic } = bodySchema.parse(request.body);
-        const memory = await prisma.memory.update({
+        let memory = await prisma.memory.findUniqueOrThrow({
+            where: {
+                id,
+            },
+        });
+        if (memory.userId !== request.user.sub) {
+            return response.status(401).send();
+        }
+        memory = await prisma.memory.update({
             where: {
                 id,
             },
@@ -79,13 +98,19 @@ export async function memoriesRoutes(app: FastifyInstance) {
         return memory;
     });
 
-    app.delete('/memories/:id', async (request) => {
+    app.delete('/memories/:id', async (request, response) => {
         const paramsSchema = z.object({
             id: z.string().uuid(),
         });
-
         const { id } = paramsSchema.parse(request.params);
-
+        const memory = await prisma.memory.findUniqueOrThrow({
+            where: {
+                id,
+            },
+        });
+        if (memory.userId !== request.user.sub) {
+            return response.status(401).send();
+        }
         await prisma.memory.delete({
             where: {
                 id,
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,19 @@
 import cors from '@fastify/cors';
+import jwt from '@fastify/jwt';
+import 'dotenv/config';
 import fastify from 'fastify';
+import { authRoutes } from './routes/auth';
 import { memoriesRoutes } from './routes/memories';
 
 const app = fastify();
+
 app.register(cors, {
     origin: true,
 });
+app.register(jwt, {
+    secret: 'spacetime',
+});
+app.register(authRoutes);
 app.register(memoriesRoutes);
 
 app.listen({
